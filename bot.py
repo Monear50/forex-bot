024b930ef29024aa5119039181e472c25dd37216
Monear50 <160772053+Monear50@users.noreply.github.com>
2026-03-11 15:22:19 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,51 @@
+import requests
 import time
+import statistics
+
+# Price API
+pairs = {
+    "BTCUSDC": "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDC",
+    "XAUUSD": "https://api.binance.com/api/v3/ticker/price?symbol=XAUUSDT"
+}
+
+price_history = {
+    "BTCUSDC": [],
+    "XAUUSD": []
+}
+
+def get_price(url):
+    data = requests.get(url).json()
+    return float(data["price"])
 
 while True:
-    print("Bot Running...")
-    time.sleep(5)
+
+    for pair, url in pairs.items():
+
+        price = get_price(url)
+
+        history = price_history[pair]
+        history.append(price)
+
+        if len(history) > 20:
+            history.pop(0)
+
+        if len(history) >= 10:
+
+            ma_short = statistics.mean(history[-5:])
+            ma_long = statistics.mean(history)
+
+            print(f"\nPair: {pair}")
+            print("Current Price:", price)
+            print("MA Short:", ma_short)
+            print("MA Long:", ma_long)
+
+            if ma_short > ma_long:
+                print("Signal: BUY")
+
+            elif ma_short < ma_long:
+                print("Signal: SELL")
+
+            else:
+                print("Signal: WAIT")
+
+    time.sleep(15)
